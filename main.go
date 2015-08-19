cbc8c22556cb547dabc30dfd41b1b404346c11b1
Didip Kerabat <didip@newrelic.com>
2015-08-19 16:17:14 -0700

    Update Agent docs.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "net/http"
 
 func main() {
+	println("Running HTTP Server on port: 3000")
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.ListenAndServe(":3000", nil)
 }
